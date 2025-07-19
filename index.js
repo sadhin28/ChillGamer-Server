ef96612b4fb59b1dab0a440739967d1206f3e458
sadhin28 <taosifsadhin113@gmail.com>
2025-07-19 23:28:18 +0600

    watch list delate operatin dene

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('Chill Gamer Website is running')
 })
 //===============================
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrsefbd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +28,7 @@ async function run() {
         //create database
         const usersCollection = client.db('ChillGamer-User').collection('Users')
         const GameReviewCollection = client.db("GameReview").collection('Reviews')
+        const WatchCollection = client.db("WatchList").collection('WatchItems')
 
         //Post a user
         app.post('/user', async (req, res) => {
@@ -42,23 +43,83 @@ async function run() {
             const result = await coursor.toArray();
             res.send(result)
         })
-  
-         //post a reviews
-         app.post('/reviews',async(req,res)=>{
+
+        //post a reviews
+        app.post('/reviews', async (req, res) => {
             const newReviews = req.body;
             res.send(newReviews);
             const result = await GameReviewCollection.insertOne(newReviews);
             res.send(result)
-         })
+        })
 
-         //get a reviews
-         app.get('/reviews',async(req,res)=>{
+        //get a reviews
+        app.get('/reviews', async (req, res) => {
             const coursor = GameReviewCollection.find();
             const result = await coursor.toArray();
             res.send(result)
-         })
-
-
+        })
+        //get review by id
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await GameReviewCollection.findOne(query)
+            res.send(result)
+        })
+        //delete reviews
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await GameReviewCollection.deleteOne(query);
+            res.send(result)
+        })
+        //update reveiw
+        app.put('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { Upsert: true };
+            const updatereview = req.body;
+            const newUpdateReview = {
+                $set: {
+                    game_image:updatereview.game_image,
+                    game_title :updatereview.game_title,
+                    review:updatereview.review,
+                    rating:updatereview.rating,
+                    publish_year:updatereview.publish_year,
+                    genres:updatereview.genres,
+                    user_email:updatereview.user_email,
+                    user_name:updatereview.user_name
+                }
+            }
+            const result = await GameReviewCollection.updateOne(filter,newUpdateReview,options);
+            res.send(result)
+        })
+        //post watch list
+        app.post('/watch_list', async (req, res) => {
+            const newReviews = req.body;
+            res.send(newReviews);
+            const result = await WatchCollection.insertOne(newReviews);
+            res.send(result)
+        })
+        //get a reviews
+        app.get('/watch_list', async (req, res) => {
+            const coursor = WatchCollection.find();
+            const result = await coursor.toArray();
+            res.send(result)
+        })
+        //get watch list
+        app.get('/watch_list/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:new ObjectId(id)};
+            const result =await WatchCollection.findOne(query);
+            res.send(result)
+        })
+        //delete watch list by id
+         app.delete('/watch_list/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await WatchCollection.deleteOne(query);
+            res.send(result)
+        })
         //Get last loging by email
         app.patch('/user', async (req, res) => {
             const email = req.body.email;
@@ -71,7 +132,7 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc)
         })
 
-       
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
